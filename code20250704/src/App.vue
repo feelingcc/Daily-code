16a8777ae4ee895d84ffa672758e8f0183e66a27
ccc777777 <3027738783@qq.com>
2025-07-04 22:24:27 +0800

    code20250704

---
+++ code20250704/src/App.vue
@@ -0,0 +1,14 @@
+<script setup>
+// vbase
+import { ref } from 'vue'
+const str = ref('<span style="color:red;">Hello Vue</span>')
+</script>
+
+<template>
+	<div>
+		<p v-html="str"></p>
+		<p v-text="str"></p>
+	</div>
+</template>
+
+<style scoped></style>
