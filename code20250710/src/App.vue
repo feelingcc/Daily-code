1986e397275a93fdf6cbc821bda35e9f3b19bfa4
ccc777777 <3027738783@qq.com>
2025-07-10 21:48:26 +0800

    code20250710

---
+++ code20250710/src/App.vue
@@ -0,0 +1,33 @@
+<script setup>
+import Item from './component/Item.vue';
+// 提供数据
+const goodsObj = {
+	id: '3998274',
+	name: '德国百年⼯艺⾼端⽔晶玻璃红酒杯2⽀装',
+	price: 139,
+	picture:
+		'https://yanxuan-item.nosdn.127.net/8896b897b3ec6639bbd1134d66b9715c.jpg'
+}
+</script>
+
+<template>
+	<div class="list">
+		<item :goodsObjPircture="goodsObj.picture" :goodsObjName="goodsObj.name" :goodsObjPrice="goodsObj.price"></item>
+		<!-- <item></item> -->
+	</div>
+</template>
+
+<style lang="scss">
+* {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+}
+
+.list {
+	width: 1000px;
+	margin: 0 auto;
+	display: flex;
+	flex-wrap: wrap;
+}
+</style>
---
+++ code20250710/src/component/Item.vue
@@ -0,0 +1,52 @@
+<script setup>
+
+defineProps(['goodsObjPircture', 'goodsObjName', 'goodsObjPrice'])
+</script>
+
+<template>
+    <div class="item">
+        <img :src="goodsObjPircture" :alt="goodsObjName" />
+        <p class="name">{{ goodsObjName }}</p>
+        <p class="price">{{ goodsObjPrice }}</p>
+    </div>
+</template>
+
+<style scoped lang="scss">
+.item {
+    width: 240px;
+    margin-left: 10px;
+    padding: 20px 30px;
+    transition: all 0.5s;
+    margin-bottom: 20px;
+
+    .item:nth-child(4n) {
+        margin-left: 0;
+    }
+
+    &:hover {
+        box-shadow: 0px 0px 5px rgba(0, 0, 0, 0.2);
+        transform: translate3d(0, -4px, 0);
+        cursor: pointer;
+    }
+
+    img {
+        width: 100%;
+    }
+
+    .name {
+        font-size: 18px;
+        margin-bottom: 10px;
+        color: #666;
+    }
+
+    .price {
+        font-size: 22px;
+        color: firebrick;
+    }
+
+    .price::before {
+        content: '¥';
+        font-size: 22px;
+    }
+}
+</style>
