81edec6d58c24a7a7de621093243bc1aa6594a0b
ccc777777 <3027738783@qq.com>
2025-07-14 23:13:22 +0800

    code20250714

---
+++ code20250714/src/App.vue
@@ -0,0 +1,94 @@
+<script setup>
+import { nextTick, ref } from 'vue'
+
+const isEidt = ref(false)
+const inputRef = ref(null)
+const showEidt = async () => {
+	isEidt.value = true
+	await nextTick()
+	inputRef.value.focus()
+}
+</script>
+
+<template>
+	<div class="box">
+		<h3>标题</h3>
+		<button @click="showEidt">编辑</button>
+	</div>
+
+	<div v-if="isEidt">
+		<input type="text" ref="inputRef">
+	</div>
+</template>
+
+<style scoped>
+.box {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	width: 200px;
+	height: 40px;
+}
+</style>
+
+<!-- <script setup>
+import { ref, onMounted } from 'vue';
+
+const pRef = ref(null)
+
+onMounted(() => {
+	console.log(`output->pRef`, pRef.value)
+})
+</script>
+
+<template>
+	<div>
+		<p ref="pRef">Hello World</p>
+	</div>
+</template>
+
+<style scoped></style> -->
+
+<!-- <script setup>
+import { ref } from 'vue';
+import SelectInput from './component/SelectInput.vue';
+
+
+const currentSelect = ref("BJ")
+</script>
+
+<template>
+	<select-input v-model="currentSelect"></select-input>
+</template>
+
+<style scoped></style> -->
+
+<!-- <script setup>
+import { ref } from 'vue';
+
+const msg = ref("")
+</script>
+
+<template>
+	<div>
+		<p>{{ msg }}</p>
+		<input type="text" :value="msg" @input="msg = $event.target.value">
+	</div>
+</template>
+
+<style scoped></style> -->
+
+<!-- <script setup>
+import { ref } from 'vue';
+
+const msg = ref("")
+</script>
+
+<template>
+	<div>
+		<p>{{ msg }}</p>
+		<input type="text" v-model="msg">
+	</div>
+</template>
+
+<style scoped></style> -->
---
+++ code20250714/src/component/SelectInput.vue
@@ -0,0 +1,16 @@
+<script setup>
+const currentSelect = defineModel()
+</script>
+
+<template>
+    <div class="select-input-wrap">
+        <select v-model="currentSelect">
+            <option value="BJ">北京</option>
+            <option value="SH">上海</option>
+            <option value="GZ">广州</option>
+            <option value="SZ">深圳</option>
+        </select>
+    </div>
+</template>
+
+<style scoped></style>
