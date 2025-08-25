d2823ff74eb2fdb415b6afe7b5681352d2bdf074
ccc777777 <3027738783@qq.com>
2025-08-25 19:17:23 +0800

    hash table end

--- code20250823/hash.hpp
+++ code20250823/hash.hpp
@@ -3,8 +3,9 @@
 #include <iostream>
 #include <vector>
 #include <utility>
+#include <string>
 
-namespace hash_table {
+namespace hash_bucket {
     inline unsigned long __stl_next_prime(unsigned long n)  {
         // Note: assumes long is at least 32 bits.
         static const int __stl_num_primes = 28;
@@ -22,6 +23,22 @@ namespace hash_table {
         return pos == last ? *(last - 1) : *pos;
     }
 
+    template<typename Key>
+    struct HashK {
+        size_t operator()(const Key& key) { return (size_t)key; }
+    };
+
+    template<>
+    struct HashK<std::string> {
+        size_t operator()(const std::string& key) {
+            size_t res = 0;
+            for (auto c : key) {
+                res += c * 131;
+            }
+            return res;
+        }
+    };
+
     template<typename Key , typename Value>
     struct HashTableNode {
         std::pair<Key , Value> data;
@@ -29,15 +46,137 @@ namespace hash_table {
         HashTableNode(const std::pair<Key , Value>& _data) :data(_data) , next(nullptr) {}
     };
 
-    template<typename Key , typename Value>
+    template<typename Key>
+    struct IsEqualKey {
+        bool operator()(const Key& k1 , const Key& k2) {
+            return k1 == k2;
+        }
+    };
+
+    //  前置声明
+    //  默认参数只能在声明时写，不能在定义时写
+    template<typename Key , typename Value , typename HashKey = HashK<Key> , typename EqualKey = IsEqualKey<Key>>
+    class HashTable;
+
+    template<typename Key , typename Value , typename Ref , typename Ptr , typename HashKey = HashK<Key> , typename EqualKey = IsEqualKey<Key>>
+    struct HashIterator {
+        using HashNode = HashTableNode<Key , Value>;
+        using Ht = HashTable<Key , Value , HashKey , EqualKey>;
+        using Self = HashIterator<Key , Value , Ref , Ptr , HashKey , EqualKey>;
+        HashNode* cur;
+        Ht* ht;
+        HashIterator(HashNode* node , Ht* _ht) :cur(node) , ht(_ht) {}
+        // 前置++
+        Self& operator++() {
+            if (cur->next) {
+                cur = cur->next;
+            } else {
+                size_t key = HashKey()(cur->data.first) % ht->table.size();
+                key++;
+                while (key < ht->table.size() && !ht->table[key]) {
+                    key++;
+                }
+                if (key == ht->table.size()) {
+                    cur = nullptr;
+                } else {
+                    cur = ht->table[key];
+                }
+            }
+            return *this;
+        }
+
+        Ref operator* () {
+            return cur->data;
+        }
+
+        Ptr operator-> () {
+            return &(cur->data);
+        }
+
+        bool operator== (const Self& s) {
+            return cur == s.cur;
+        }
+        
+        bool operator!= (const Self& s) {
+            return cur != s.cur;
+        }
+    };
+
+    // HashKey仿函数：将key转换为无符号整数
+    // EqualKey仿函数：key需要支持 == 运算符
+    template<typename Key , typename Value , typename HashKey , typename EqualKey>
     class HashTable {
+        // 内层友元模板，不能和外层HashTable模板参数相同
+        template<typename K2 , typename V2 , typename Ref2 , typename Ptr2 , typename HK2 , typename EK2>
+        friend struct HashIterator;
+
         using HashNode = HashTableNode<Key , Value>;
         public:
+        typedef HashIterator<Key , Value , std::pair<Key , Value>& , std::pair<Key , Value>* , HashKey , EqualKey> iterator;
+            // using iterator = ;
+            using const_iterator = HashIterator<Key , Value , const std::pair<Key , Value>& ,  const std::pair<Key , Value>* , HashKey , EqualKey>;
+
+            iterator begin() {
+                if (n == 0) {
+                    return end();
+                }
+
+                for (size_t i = 0; i < table.size(); i++) {
+                    if (table[i]) {
+                        return iterator(table[i], this);
+                    }
+                }
+
+                return end();
+            }
+
+            iterator end() {
+                return iterator(nullptr , this);
+            }
+
             HashTable()
                 :table(__stl_next_prime(0)) , n(0)
             {}
 
-            bool insert (std::pair<Key , Value>& data) {
+            ~HashTable() {
+                for (size_t i = 0; i < table.size(); i++) {
+                    HashNode* cur = table[i];
+                    while (cur) {
+                        HashNode* next = cur->next;
+                        delete cur;
+                        cur = next;
+                    }
+                    table[i] = nullptr;
+                }
+            }
+
+            HashTable(const HashTable& ht) {
+               table.resize(ht.table.size());
+               n = ht.n;
+               for (size_t i = 0; i < ht.table.size(); i++) {
+                    HashNode* cur = ht.table[i];
+                    if (!cur) {
+                        table[i] = nullptr;
+                        continue;
+                    }
+                    while (cur) {
+                        HashNode* newnode = new HashNode(cur->data);
+                        newnode->next = table[i];
+                        table[i] = newnode;
+                        cur = cur->next;
+                    }
+                }
+            }
+            
+            HashTable& operator=(HashTable ht) {
+                table.swap(ht.table);
+                std::swap(n  , ht.n);
+                return *this;
+            }
+
+            bool insert (const std::pair<Key , Value>& data) {
+                if (find(HashKey()(data.first))) return false; // 存在相同元素，不插入
+
                 // 负载因子为1时，hash需要扩容
                 if (n == table.size()) {
                     std::vector<HashNode*> newTable(__stl_next_prime(table.size() + 1));
@@ -46,7 +185,7 @@ namespace hash_table {
                         HashNode* cur = head;
                         while (cur) {
                             HashNode* next = cur->next;
-                            size_t key = cur->data.first % newTable.size();
+                            size_t key = HashKey()(cur->data.first) % newTable.size();
                             cur->next = newTable[key];
                             newTable[key] = cur;
                             cur = next;
@@ -58,16 +197,53 @@ namespace hash_table {
                 }
                 // 添加新节点
                 HashNode* newnode = new HashNode(data);
-                size_t key = data.first % table.size();
+                size_t key = HashKey()(data.first) % table.size();
                 newnode->next = table[key];
                 table[key] = newnode;
                 n++;
 
                 return true;
             }
+
+            bool erase(const Key& key) {
+                size_t hashIndex = HashKey()(key) % table.size();
+                HashNode* prev = nullptr;
+                HashNode* cur = table[hashIndex];
+                
+                while (cur) {
+                    // 删除需要区分头删还是中间节点删除
+                    if (EqualKey()(HashKey()(cur->data.first) , HashKey()(key))) {
+                        if (!prev) {
+                            // 头删
+                            table[hashIndex] = cur->next;
+                        } else {
+                            // 中间节点删除
+                            prev->next = cur->next;
+                        }
+                        delete cur;
+                        return true;
+                    } else {
+                        // 迭代继续寻找
+                        prev = cur;
+                        cur = cur->next;
+                    }
+                }
+                return false;
+            }
+
+            bool find (const Key& key) {
+                size_t hashIndex = HashKey()(key) % table.size();
+                HashNode* cur = table[hashIndex];
+                while (cur) {
+                    if (EqualKey()(HashKey()(cur->data.first) , HashKey()(key))) return true;
+                    cur = cur->next;
+                }
+                return false;
+            }
+            
         private:
             std::vector<HashNode*> table;
             size_t n;
     };
 
-} // end namespace hash_table
+} // end namespace hash_bucket
--- code20250823/test.cc
+++ code20250823/test.cc
@@ -1,7 +1,62 @@
 #include "hash.hpp"
 
-int main () {
+void print(hash_bucket::HashTable<int, int> &h)
+{
+    auto it = h.begin();
+    while (it != h.end())
+    {
+        std::cout << it->first << " " << it->second << std::endl;
+        ++it;
+    }
+    std::cout << "---------------------------------" << std::endl;
+}
 
-    
+void test1()
+{
+    hash_bucket::HashTable<int, int> h;
+    int a[] = {3, 1, 6, 7, 888, 288, 188, 1, 5, 6, 7, 6};
+    for (auto e : a)
+        h.insert({e, e});
+
+    print(h);
+
+    h.erase(1);
+    print(h);
+    h.erase(3);
+    print(h);
+    h.erase(5);
+    print(h);
+    h.erase(6);
+    print(h);
+    h.erase(7);
+    print(h);
+    h.erase(288);
+    print(h);
+    h.erase(188);
+    print(h);
+    h.erase(88);
+    print(h);
+}
+
+void test2 () {
+    hash_bucket::HashTable<int, int> h;
+    int a[] = {3, 1, 6, 7, 888, 288, 188, 1, 5, 6, 7};
+    for (auto e : a)
+        h.insert({e, e});
+    for (int i = 0; i < 1000; i++) {
+        h.insert({ i ,  i});
+    }
+    hash_bucket::HashTable<int, int> hh(h);
+    print(h);
+    hh.erase(1);
+    print(hh);
+}
+
+int main()
+{
+
+    // test1();
+    test2();
+
     return 0;
 }
