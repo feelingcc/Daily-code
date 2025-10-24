0ae207224f97641c2fdb442d378ae2d231550d01
ccc777777 <3027738783@qq.com>
2025-10-24 22:41:33 +0800

    pthread mutex

---
+++ code20251024/test.cc
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <pthread.h>
+
+pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
+
+int ticket = 100;
+void *route(void *arg)
+{
+    char *id = (char *)arg;
+    while (1)
+    {
+        pthread_mutex_lock(&mutex);
+        if (ticket > 0)
+        {
+            usleep(1000);
+            printf("%s sells ticket:%d\n", id, ticket);
+            ticket--;
+            pthread_mutex_unlock(&mutex);
+        }
+        else
+        {
+            pthread_mutex_unlock(&mutex);
+            break;
+        }
+    }
+
+    return nullptr;
+}
+int main(void)
+{
+    pthread_t t1, t2, t3, t4;
+
+    pthread_create(&t1, NULL, route, (void *)"thread 1");
+    pthread_create(&t2, NULL, route, (void *)"thread 2");
+    pthread_create(&t3, NULL, route, (void *)"thread 3");
+    pthread_create(&t4, NULL, route, (void *)"thread 4");
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    pthread_join(t3, NULL);
+    pthread_join(t4, NULL);
+
+    return 0;
+}
