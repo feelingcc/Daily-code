a7d361422313ad82f5dbe7b0c36bfa93e97dd229
ccc777777 <3027738783@qq.com>
2026-01-15 15:57:04 +0800

    cmake test static lib

---
+++ code20260115/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include "math.h"
+// #include "../my_lib/include/math.h"
+
+int main() {
+
+    std::cout << add(10 , 20) << std::endl;
+    std::cout << sub(10 , 20) << std::endl;
+
+    return 0;
+}
