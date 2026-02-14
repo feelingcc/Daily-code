19707803f58cdccd85033c5c944552d5dd2a2e60
ccc777777 <3027738783@qq.com>
2026-02-14 19:32:29 +0800

    redis-plus-plus的基本使用

---
+++ code20260214/redis-plus-plus.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <string>
+#include <sw/redis++/redis++.h>
+
+int main() {
+
+    sw::redis::Redis redis("tcp://127.0.0.1:6379");
+    std::string result = redis.ping();
+    std::cout << result << std::endl;
+
+    return 0;
+}
