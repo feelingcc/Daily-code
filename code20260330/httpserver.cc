daa506f819eec23d18ccf295d5fe9ebd28b35a8b
cxd <cxd@cxd-ubantu22.04>
2026-03-30 19:01:03 +0800

    httpserver

--- code20260330/httpserver.cc
+++ code20260330/httpserver.cc
@@ -22,7 +22,26 @@ void HelloWorld(const httplib::Request & req, httplib::Response & resp) {
 int main() {
 
     httplib::Server server;
-    server.Get("./hello" , HelloWorld);
+    server.Get("/hello" , HelloWorld);
+    server.Get(R"(/numbers/(\d+))" , [](const httplib::Request & req, httplib::Response & resp){
+        std::cout << req.method << std::endl;
+        std::cout << req.path << std::endl;
+        std::cout << req.body << std::endl;
+        //打印头部字段信息
+        for (auto it : req.headers) {
+            std::cout << it.first << " = " << it.second << std::endl;
+        }
+        //打印查询字符串
+        for (auto it : req.params) {
+            std::cout << it.first << " = " << it.second << std::endl;
+        }
+        // 打印捕捉参数
+        std::cout << req.matches[1] << std::endl;
+        std::string numb = req.matches[1];
+        std::string html_body = "<html><body><h1> " + numb  + "</h1></body></html>";
+        resp.set_content(html_body, "text/html");
+        resp.status = 200;
+    });
     server.listen("0.0.0.0" , 8080);
 
     return 0;
